6fe440c420418d585a2316c516c5d05c9e2c66e5
liux2770 <rachelliu219@foxmail.com>
2017-02-16 02:33:46 -0600

    change to per capita income

--- DataCleaning.R
+++ DataCleaning.R
@@ -3,9 +3,9 @@ library(reshape2)
 
 setwd('/Users/ruiqiliu/Desktop/MSBA/Others/USElection')
 
-#Read in household income data, the data can be downloaded at US Census Bureau:
-#https://www.census.gov/data/tables/time-series/demo/income-poverty/historical-income-households.html
-income_raw <- read.csv('income_raw.csv',header = TRUE)
+###Read in per capita personal income data, the data can be downloaded from FRED at:
+###https://fred.stlouisfed.org/tags/series?t=annual%3Bper+capita%3Bpersonal+income%3Bstate&ob=pv&od=desc
+income_raw <- read.csv('pcpi.csv',header = TRUE)
 
 #transform panel data to long format
 income_raw_long <- reshape2::melt(income_raw,id.vars = 'State')
@@ -24,27 +24,27 @@ income <- income_raw_long %>%
 us <- income %>% filter(state_abb == 'United States')
 relative_income <- left_join(income,us,by = 'year') %>% 
       dplyr::rename(state_abb = state_abb.x,
-             household_income= value.x ,
+             personal_income= value.x ,
              us = state_abb.y,
-             national_income = value.y) %>%
-      mutate(relative_income = household_income/national_income) %>%
-      select(state_abb,year,household_income,national_income,relative_income)
+             national_personal_income = value.y) %>%
+      mutate(relative_personal_income = personal_income/national_personal_income) %>%
+      select(state_abb,year,personal_income,national_personal_income,relative_personal_income)
 
 
 
-#Read in election data, the data can be found at official Federal Election Commission report
-election_raw <- read.csv('election_raw.csv',header = TRUE,stringsAsFactors = FALSE)
+###Read in election data, the data can be found at official Federal Election Commission report
+election_raw <- read.csv('election.csv',header = TRUE,stringsAsFactors = FALSE)
 
 #calculate margin and prepare the df ready to be merged with relative_income
 election <- election_raw %>%
-      mutate(margin = Republican_percent - Democratic_percent,
-             number_margin = Republican_number-Democratic_number,
-             win = ifelse(margin >0,'Republican','Democratic')) %>%
+      mutate(margin_percent = Democratic_percent - Republican_percent,
+             margin_votes = abs(Republican_votes-Democratic_votes),
+             win = ifelse(margin_percent >0,'Democratic','Republican')) %>%
       rename(state_abb = State)
 
 
 
-#JOIN election and relative income
+###JOIN election and relative income
 output <- left_join(election,relative_income,by = c('state_abb','year'))
 write.csv(output,'plot_df.csv',row.names = FALSE)
 
